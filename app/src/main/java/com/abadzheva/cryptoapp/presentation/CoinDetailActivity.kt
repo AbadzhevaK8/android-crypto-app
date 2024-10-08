f63a22322a3d138ffcf6f52f6aca9603c73101c4
k8abadzheva <make.me.loco@gmail.com>
2024-10-09 01:48:26 +0300

    feat: use CoinDetailFragment.kt in CoinDetailActivity.kt.

--- app/src/main/java/com/abadzheva/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/abadzheva/cryptoapp/presentation/CoinDetailActivity.kt
@@ -3,28 +3,15 @@ package com.abadzheva.cryptoapp.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ViewModelProvider
 import com.abadzheva.cryptoapp.R
 import com.abadzheva.cryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
-    private lateinit var viewModel: CoinViewModel
     private lateinit var binding: ActivityCoinDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_coin_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         // ------------------
         binding = ActivityCoinDetailBinding.inflate(layoutInflater)
         val view = binding.root
@@ -34,29 +21,18 @@ class CoinDetailActivity : AppCompatActivity() {
             finish()
             return
         }
-        val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL)
-        viewModel =
-            ViewModelProvider(
-                this,
-            )[CoinViewModel::class.java]
-        fromSymbol?.let { it ->
-            viewModel.getDetailInfo(it).observe(
-                this,
-            ) {
-                binding.tvPrice.text = it.price.toString()
-                binding.tvMinPrice.text = it.lowday.toString()
-                binding.tvMaxPrice.text = it.highday.toString()
-                binding.tvLastMarket.text = it.lastmarket
-                binding.tvLastUpdate.text = it.lastupdate
-                binding.tvFromSymbol.text = it.fromsymbol
-                binding.tvToSymbol.text = it.tosymbol
-                Picasso.get().load(it.imageurl).into(binding.ivLogoCoin)
-            }
+        val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
     companion object {
         private const val EXTRA_FROM_SYMBOL = "fSym"
+        private const val EMPTY_SYMBOL = ""
 
         fun newIntent(
             context: Context,
